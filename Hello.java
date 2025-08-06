637c69598f48cbb9a7bcc2bab907ee51f55bdf7b
TalesRL <talesleite.r@gmail.com>
2025-08-06 19:23:11 -0300

    Feat:atribuido a função olaMundo.

--- Hello.java
+++ Hello.java
@@ -1,9 +1,13 @@
 public class Hello {
     public static void main (String[]args){
         helloWorld();
+        olaMundo();
     }
 public static void helloWorld() {
     System.out.println("Hello World");
     
+}
+public static void olaMundo(){
+    System.out.println("Olá Mundo.");
 }
 }
